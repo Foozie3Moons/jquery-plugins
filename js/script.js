4fe09b01f6f33eb3bcd20fd705979cbd9e4b9e3a
foozie3moons <matthew.bell.290@gmail.com>
2017-09-06 11:00:49 -0700

    created an unofficial website for a random wikipedia article for a school

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$( function() {
+  $( "#tabs" ).tabs();
+} );
+$( function() {
+  $( "#accordion" ).accordion();
+} );
+$('.parallax-window').parallax({imageSrc: 'http://www.wrde.com/pics/crisfield.jpg'});
